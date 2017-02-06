868c9d4de631a2fd35f1a2d6d49e4a67a61c8896
wjlnhs <524717071@qq.com>
2017-02-06 17:29:01 +0800

    sixed

--- js/script.js
+++ js/script.js
@@ -1,45 +1,72 @@
 
 $(function(){
+    var mypuzzle=null;
+    var myPage=null;
+    var mypuzzleinited=false;
+    var local={
+        myPage:JSON.parse(localStorage['myPage'] || '{}'),
+        mypuzzle:JSON.parse(localStorage['mypuzzle'] || '{}')
+    }
     var puzzle=function(Type,playername){
+        this.itemid='id'+Math.random()*1e20;
         this.bgsrc=$('#puzzle').css('background-image');
-        this.$dragbox=$( "#puzzle" );
+        this.$dragbox=$("#puzzle" );
         this.$prviewbox=$( "#puzzleDestination" );
+        this.$btnpause=$('.btn-pause');
+        this.$playername=$('#playername');
         this.ondrag=false;//是否正在拖拽
         this.$compare=$();//将要比较的（预览的）
         this.$draging=$();//正在拖拽的
         this.prviewleft=this.$prviewbox.position().left;//
         this.startIndex=100;
         this.second=0;
-        this.showsecond='0:00';
-        this.$btnpause=$('.btn-pause');
-        this.$playername=$('#playername');
+        this.showsecond='0 ：00';
         this.TypeObj={
             EASY:4,
             HARD:9
         }
+
         this.Type=Type;
         this.playername=playername;
         this.ispause=false;
         this.COUNT=this.TypeObj[Type];
+
         this.init=function(){
             var This=this;
+            myPage.state='start';
             this.$playername.text(playername);
             this.initRotate();//添加旋转事件
             this.restart();
             $('.btn-restart').on('click',function(){
                 This.second=0;
+                myPage.state='beforestart';
+                 local.myPage.state='start';
+                // myPage.state='start';
                 $('#start').show();
                 $('#end').hide();
             })
             This.$btnpause.on('click',function(){
                 if(This.ispause){
-                   This.onResume();
+                    This.onResume();
                 }else{
-                   This.onPause();
+                    This.onPause();
                 }
             })
         }
         this.init();
+        //第一次加載bi并且不是重置
+        if(local.myPage.state && local.myPage.state!='beforestart' && !mypuzzleinited){
+            this.second=local.mypuzzle.second;
+            this.showsecond=local.mypuzzle.showsecond;
+            if(local.myPage.state=='pause'){
+                $('#timer span').text(local.mypuzzle.showsecond);
+                this.onPause();
+            }else if(local.myPage.state=='end'){
+                this.showEnd(true);
+            }
+
+        }
+        mypuzzleinited=true;
     }
     puzzle.prototype={
         constructor:puzzle,
@@ -58,6 +85,7 @@ $(function(){
 
         initTimer:function(){
             var This=this;
+            $('#timer span').text(This.showsecond);
             clearInterval(This.timer);
             This.timer=setInterval(function(){
                 This.second++;
@@ -74,6 +102,7 @@ $(function(){
             this.$btnpause.text('PAUSE');
             this.$dragbox.show();
             this.$prviewbox.show();
+            myPage.state='start';
         },
         onPause:function(){
             clearInterval(this.timer);
@@ -81,18 +110,28 @@ $(function(){
             this.$btnpause.text('RESUME');
             this.$dragbox.hide();
             this.$prviewbox.hide();
+            myPage.state='pause';
         },
-        showEnd:function(){
+        showEnd:function(ishistory){
+            myPage.state='end';
             $('#end').show();
             var users=JSON.parse(localStorage['users'] || '[]');
-            var cuitem={
-                second: this.second,
-                showsecond:this.showsecond,
-                playername:this.playername,
-                type:this.Type,
-                iscuitem:true
+            if(ishistory){
+                cuitem=(users.slice(-1) || [])[0];
+                cuitem.iscuitem=true;
+            }else{
+                var cuitem={
+                    second: this.second,
+                    showsecond:this.showsecond,
+                    playername:this.playername,
+                    type:this.Type,
+                }
+                users.push(cuitem);
+                localStorage.setItem('users',JSON.stringify(users));
+                cuitem.iscuitem=true;
+
             }
-            users.push(cuitem);
+
             var sortusers=users.sort(function(a,b){
                 return a.second-b.second;
             })
@@ -102,41 +141,55 @@ $(function(){
                 if(index<3){
                     html+='<tr><td>'+(index+1)+'</td><td>'+item.type+'</td><td>'+item.playername+'</td><td>'+item.showsecond+'</td></tr>'
                 }
-                if(item.iscuitem){
+                if(item.iscuitem ){
                     cuitemhtml+='<tr><td>'+(index+1)+'</td><td>'+item.type+'</td><td>'+item.playername+'</td><td>'+item.showsecond+'</td></tr>'
                 }
             })
             delete cuitem.iscuitem;
             html+=cuitemhtml;
             $('#end tbody').html(html);
-            localStorage.setItem('users',JSON.stringify(users));
+
         },
         restart:function(){
             var This=this;
             this.onResume.call(this);
-            this.$blanks=this.creatdiv($('#puzzle'));
-            this.$blanksPrview=this.creatdiv(this.$prviewbox,true);
+            //不是第一次进入 并且不是开始前状态并且不是通过restart按钮重置游戏
+            if(local.myPage.state && local.myPage.state!='beforestart' && !mypuzzleinited){
+                $('#puzzle').html(local.mypuzzle.puzzleHTML).css('background-image','none');
+                if(local.myPage.state!='end'){
+                    this.$prviewbox.html(local.mypuzzle.puzzleDestinationHTML).css('background-image','none');
+                  //  this.showEnd(true);
+                }
+            }else{
+                this.$blanks=this.creatdiv($('#puzzle'));
+                this.$blanksPrview=this.creatdiv(this.$prviewbox,true);
+            }
             this.$dragbox.sortable({
+                helper:'original',
                 connectWith: '#puzzle',
                 placeholderType:false,
                 start :function(event, ui){
                     if(This.ispause){
                         return false;
                     }
-                    This.ondrag=true;
+
                     This.$draging=ui.item;
                     $('.ui-sortable-handle').removeClass('undrag');
                     var sortindex=This.$draging.attr('sortindex');
                     This.$compare=This.$prviewbox.find('[sortindex='+sortindex+']');
-                    This.$draging.addClass('isdraging');
-                    This.$draging.attr('rotate')
                     This.$draging.css({
                         transform:'rotateZ('+This.$draging.attr('rotate')+'deg) scale(1.1)'
                     });
-                    console.log(ui)
+                    setTimeout(function(){
+                        This.ondrag=true;
+                        console.log(ui)
+                        This.$draging.addClass('isdraging');
+                    })
+                  // This.$draging.attr('rotate');
+
                 },
                 sort:function(event, ui){
-                    console.log(This.$draging.css('left'))
+                    console.log(This.$draging.css('left'));
                     console.log(ui)
                 },
                 beforeStop:function(event, ui){
@@ -168,6 +221,7 @@ $(function(){
                                             'background-repeat':'no-repeat',
                                             'background-size':'500px 500px'
                                         })
+
                                         This.showEnd();
                                     }
 
@@ -191,17 +245,19 @@ $(function(){
         },
         initRotate:function(){
             var This=this;
+            $(document).off('keydown');
             $(document).on('keydown',function(ev){
                 if(!This.ondrag){
                     return;
                 }
-                if(ev.keyCode==37){//左
+                console.log(This.$draging.attr('rotate'))
+                if(ev.keyCode==39){//右
                     var nextRotate=parseInt(This.$draging.attr('rotate'))+90;
                     This.$draging.css({
                         transform:'rotateZ('+nextRotate+'deg) scale(1.1)'
                     })
                     This.$draging.attr('rotate',nextRotate)
-                }else if(ev.keyCode==39){//右
+                }else if(ev.keyCode==37){//左
                     var nextRotate=parseInt(This.$draging.attr('rotate'))-90;
                     This.$draging.css({
                         transform:'rotateZ('+nextRotate+'deg) scale(1.1)'
@@ -275,14 +331,36 @@ $(function(){
     var Page=function(){
         this.UserName='';
         this.Type='';
+        this.bgsrc=$('#puzzle').css('background-image');
         this.init=function(){
             this.initEvent();
+            this.initInfo();
+            this.watch();
         }
         this.second=0;
+        this.state='beforestart';
+        $.extend(this,local.myPage);
         this.init();
+$('html').css('visibility','visible')
+
     }
     Page.prototype={
         constructor:Page,
+        initInfo:function(){
+            //姓名
+            $('#name').val(this.UserName);
+            //难度
+            var jsonType={
+                'EASY':1,
+                'HARD':2
+            }
+            $('#difficult').val(jsonType[this.Type] || 1);
+            //图片
+            $('#puzzle, #puzzleDestination, .preview').css({
+                'background-image':'url("'+this.bgsrc+'")',
+                'background-size':'100% 100%'
+            });
+        },
         initEvent:function(){
             var This=this;
             //拖拽
@@ -317,6 +395,7 @@ $(function(){
                 var reader = new FileReader();
                 reader.onload = function(e) {
                     //oImg.src = this.result;
+                    This.bgsrc=this.result;
                     $('#puzzle, #puzzleDestination, .preview').css({
                         'background-image':'url("'+this.result+'")',
                         'background-size':'100% 100%'
@@ -336,13 +415,36 @@ $(function(){
                     'background-image':$('.preview').css('background-image'),
                     'background-size':'100% 100%'
                 });
+
                 This.Type=$('#difficult option:selected').text();
                 This.UserName=username;
-                var mypuzzle=new puzzle(This.Type,username);
+                clearInterval((mypuzzle || {}).timer);
+                mypuzzle=new puzzle(This.Type,username);
                 $('#start').hide();
                 return false;
             })
+
+        },
+        watch:function(){
+            var This=this;
+            $('#name').on('input propertychange',function(){
+                This.UserName=$.trim($(this).val());
+            })
+            $('#difficult').on('change',function(){
+                This.Type=$('#difficult option:selected').text()
+            })
         }
     }
-    var myPage=new Page();
+    myPage=new Page();
+    if(local.myPage.state && local.myPage.state!='beforestart'){
+        $('#submit').trigger('click');
+    }
+    window.onbeforeunload=function(){
+        if(mypuzzleinited){
+            mypuzzle.puzzleHTML=$('#puzzle').html();
+            mypuzzle.puzzleDestinationHTML=$('#puzzleDestination').html();
+        }
+        localStorage.setItem('myPage',JSON.stringify(myPage));
+        localStorage.setItem('mypuzzle',JSON.stringify(mypuzzle));
+    }
 })
